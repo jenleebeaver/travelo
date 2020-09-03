0623d7d1113d38e19f3270d08cdb954d22eef655
jenleebeaver <jen.lee.beaver@gmail.com>
2020-09-03 15:18:42 +0530

    Update location.rb
    Accepting attributes for :posts

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
     has_many :posts 
     has_many :users, through: :posts
-    accepts_nested_attributes_for :users
+    accepts_nested_attributes_for :users, :posts
 
     def user_attributes=(user_attributes)
       self.user = User.find_or_create_by(email: user_attributes[:email]) unless user_attributes[:email].blank?
