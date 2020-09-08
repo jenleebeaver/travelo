2c5004c863a6b3ee82a16e12cdd338cbf4799dad
jenleebeaver <jen.lee.beaver@gmail.com>
2020-09-08 14:22:49 +0530

    Debugging
    Overriding -ssl and debugging facebooksessions_controller.  Trying to associate current_user to facebooksessions_controller.

--- app/controllers/facebooksessions_controller.rb
+++ app/controllers/facebooksessions_controller.rb
@@ -3,14 +3,14 @@ class FacebooksessionsController < ApplicationController
   skip_before_action :verified_user, only: [:new, :create, :auth]
 
     def create
-      @user = User.find_or_create_by(uid: auth['uid']) do |u|
-        u.full_name = auth['info']['full_name']
+      @user = User.find_or_create_by(id: auth['uid']) do |u|
+        u.full_name = auth['info']['name']
         u.email = auth['info']['email']
-        u.image = auth['info']['image']
+        # u.image = auth['info']['image']
       end
-      login(@user)
-      session[:user_id] = @current_user
-      render '/posts/new'
+      session[:user_id] = @user.id
+      current_user 
+      render 'posts/new'
     end
    
     private
