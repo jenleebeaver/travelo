045ab643344df9a9f8a9f6bcc4ad1a47b22a3271
jenleebeaver <jen.lee.beaver@gmail.com>
2020-09-11 20:04:15 +0530

    Created randomly generated password and username
    Created randomly generated password and username for facebooksession_controller to populate @user in order to pass user model validations. Also, added a  _form into facebooksessions_controller.

--- app/controllers/facebooksessions_controller.rb
+++ app/controllers/facebooksessions_controller.rb
@@ -1,17 +1,25 @@
 
 class FacebooksessionsController < ApplicationController
-  skip_before_action :verified_user, only: [ :create, :auth]
+  skip_before_action :verified_user, only: [:new, :create, :auth]
 
-    def create
-      @user = User.find_or_create_by(uid: auth['uid']) do |u|
-        u.full_name = auth['info']['name']
-        u.email = auth['info']['email']
-        # u.image = auth['info']['image']
-      end
-      session[:user_id] = @user.uid
-      # @user.id = current_user 
-      render 'posts/new'
+  def new
+    @user = User.new
+    render 'posts/new'
+  end
+
+  def create
+    rand_password=('0'..'z').to_a.shuffle.first(8).join
+    rand_username=('0'..'z').to_a.shuffle.first(8).join
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.full_name = auth['info']['name']
+      u.email = auth['info']['email']
+      # u.image = auth['info']['image']
+      u.password = rand_password
+      u.username = rand_username
     end
+    session[:user_id] = @user.id
+    render 'posts/new'
+   end
    
     private
    
